63139304f199b487805a30f460e4b5f39a8641d6
Lina Shamoon <shamoonlina@gmail.com>
2018-05-21 23:52:30 +0200

    removed javascript

--- js/index.js
+++
@@ -1,7 +0,0 @@
-//simple jQuery Parallax Mouse Movement 
-// by Hugh Balboa
-$('#Parallax-content').mousemove(function(e) {
-   var amountMovedX = (e.pageX * -0.3 / 6);
-   var amountMovedY = (e.pageY * -0.3 / 6);
-   $(this).css('background-position', amountMovedX + 'px ' + amountMovedY + 'px ');
-});
